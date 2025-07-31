ae46620c5b00d05da1fe3f0af2add8024b07346f
Adrian Bengolea <abengolea1@gmail.com>
2025-07-31 22:29:58 +0000

    AGREGEMOS AL MEDICO LA POSIBILIDAD DE CARGAR ALGUN ESTUDIO VIA LA WEB -.

--- src/app/dashboard/studies/page.tsx
+++ src/app/dashboard/studies/page.tsx
@@ -1,3 +1,4 @@
+
 import Image from "next/image";
 import { patients, studies } from "@/lib/data";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
